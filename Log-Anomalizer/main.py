53e9d0794f33db18c12d9ff2f726c3167932e0d0
Chriss Philip Saji <chrissattasseril16@gmail.com>
2026-02-18 14:55:11 +0530

    Add log analyzer to count and filter words

---
+++ Log-Anomalizer/main.py
@@ -0,0 +1,26 @@
+from collections import Counter
+import re
+
+inputfile = "test.log"
+outputfile = "output.txt" 
+
+counts = Counter()
+lines = []
+
+word_pattern = re.compile(r"\b[a-zA-Z0-9]+\b")
+
+with open(inputfile, "r") as f:
+    for line in f:
+        line_lower = line.lower()
+        lines.append(line_lower)
+
+        words = word_pattern.findall(line_lower)
+        counts.update(words)
+
+
+with open(outputfile, "w") as out:  
+    for k, v in counts.items():
+        if v < (len(lines) * 0.01):
+            for line in lines:
+                if k in line.lower().split():
+                    out.write(line)   
